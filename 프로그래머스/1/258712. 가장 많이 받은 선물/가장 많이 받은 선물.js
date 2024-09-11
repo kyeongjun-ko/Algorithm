6335d926366fcc1dbb41e930ba8f05eb033a485e
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-09-11 14:58:29 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 30.16 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,97 @@
+function solution(friends, gifts) {
+    var answer = 0;
+    const giftMap = {}
+    
+    friends.forEach(name => {
+        giftMap[name] = {
+            friends: {},
+            gived: 0,
+            recieved: 0,
+            gift: 0,
+        }
+    })
+    
+    gifts.forEach(e => {
+        const [givenFriend, recievedFriend] = e.split(" ");
+        
+        giftMap[givenFriend].gived += 1;
+        giftMap[recievedFriend].recieved += 1;
+        
+        if (giftMap[givenFriend].friends.hasOwnProperty(recievedFriend)) {
+            giftMap[givenFriend].friends[recievedFriend] += 1;
+        } else {
+            giftMap[givenFriend].friends[recievedFriend] = 1;
+        }
+    })
+    
+    const getGiftScore = (friend) => {
+        return giftMap[friend].gived - giftMap[friend].recieved;
+    }
+    
+    const compareScore = (friend1, friend2) => {
+        if (giftMap[friend1].friends[friend2] ||
+            giftMap[friend2].friends[friend1]
+        ) {
+            if (!giftMap[friend1].friends[friend2]) {
+                giftMap[friend2].gift += 1;
+                
+                return;
+            }
+            
+            if (!giftMap[friend2].friends[friend1]) {
+                giftMap[friend1].gift += 1;
+                
+                return;
+            }
+            
+            if (giftMap[friend1].friends[friend2] > giftMap[friend2].friends[friend1]) {
+                giftMap[friend1].gift += 1;
+                
+                return;
+            }
+            
+            if (giftMap[friend1].friends[friend2] < giftMap[friend2].friends[friend1]) {
+                giftMap[friend2].gift += 1;
+                
+                return;
+            }
+            
+            if (giftMap[friend1].friends[friend2] = giftMap[friend2].friends[friend1]) {
+                if (getGiftScore(friend1) === getGiftScore(friend2)) return; 
+                
+                if (getGiftScore(friend1) > getGiftScore(friend2)) {
+                   giftMap[friend1].gift += 1;
+                }
+                
+                if (getGiftScore(friend1) < getGiftScore(friend2)) {
+                    giftMap[friend2].gift += 1;
+                }
+            }
+        } else {
+            if (getGiftScore(friend1) === getGiftScore(friend2)) return; 
+            
+            if (getGiftScore(friend1) > getGiftScore(friend2)) {
+                giftMap[friend1].gift += 1;
+            }
+                
+            if (getGiftScore(friend1) < getGiftScore(friend2)) {
+                giftMap[friend2].gift += 1;
+            }
+        }    
+    };
+    
+    for (let i = 0; i < friends.length - 1; i++) {
+        for (let j = i + 1; j < friends.length; j++) {
+            compareScore(friends[i], friends[j]);
+        }
+    }
+    
+    Object.values(giftMap).map(e => {
+        if (answer < e.gift) {
+            answer = e.gift;
+        }
+    })
+    
+    console.log("giftMap", giftMap)
+    return answer;
+}
