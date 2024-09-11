cb97b866a65b32e1c281b5770a2324e6a7f22a5a
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-09-11 13:34:13 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 2.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -0,0 +1,73 @@
+function solution(video_len, pos, op_start, op_end, commands) {
+    const stringToSeconds = (timeString) => {
+        const [minutes, seconds]  = timeString.split(":").map(Number);
+        
+        return minutes * 60 + seconds;
+    }
+    
+    const secondsToString = (seconds) => {
+        const minString = Math.floor(seconds / 60).toString().padStart(2, "0");
+        const secString = (seconds % 60).toString().padStart(2, "0");
+        
+        return `${minString}:${secString}`;
+    }
+    
+    const videoTime = stringToSeconds(video_len);
+    const startOpenningTime = stringToSeconds(op_start);
+    const endOpenningTime = stringToSeconds(op_end);
+
+    
+    const getCurrentTime = (time) => {
+        const posTime = stringToSeconds(pos);
+        
+        if (posTime >= startOpenningTime &&
+            posTime <= endOpenningTime) {
+            return endOpenningTime;
+        }
+        
+        return posTime;
+    }
+    
+    let currentTime = getCurrentTime(pos);
+    
+    const prevTime = (time) => {
+        if (time < 10) {
+            return 0;
+        }
+        
+        return time - 10;
+    }
+    
+    const nextTime = (time) => {
+        const restTime = videoTime - time;
+        
+        if (restTime < 10) {
+            return videoTime;
+        }
+        
+        return time + 10;
+    }
+    
+    const skipOpenning = (time) => {
+        if (time >= startOpenningTime &&
+            time <= endOpenningTime) {
+            return endOpenningTime;
+        }
+        
+        return time;
+    }
+    
+    commands.forEach(task => {
+        if (task === "next") {
+            currentTime = skipOpenning(nextTime(currentTime));
+        }
+        
+        if (task === "prev") {
+            currentTime = skipOpenning(prevTime(currentTime));
+        }
+    })
+    
+    console.log("result", secondsToString(skipOpenning(currentTime)));
+    
+    return secondsToString(skipOpenning(currentTime));
+}
