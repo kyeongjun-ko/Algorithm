bac27d1c0c5f24eef9ab3ca832ed38292ab0632e
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-09-25 15:54:05 +0900

    [level 3] Title: 등굣길, Time: 1.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,54 +1,30 @@
 function solution(m, n, puddles) {
-    const dp = [...new Array(n)].map(e => new Array(m).fill(0));
+    const visited = new Array(n).fill([...new Array(m)].fill(0));
+    console.log("visited", visited)
+    const search = [ [[0,0], 0, visited] ];
+    let moveCount = Infinity;
 
-    for (let i = 0; i < puddles.length; i++) {
-        [dangerCol, dangerRow] = puddles[i];
+//     while (search.length !== 0) {
+//         const target = search.shift();
+//         console.log("target", target);
+//         const [[x, y], currentCount, currentVisit] = target;
         
-        dp[dangerRow - 1][dangerCol - 1] = -1;
-    };
-    
-    dp[0][0] = 1;
-    
-    for (let row = 0; row < n; row++) {
-        for (let col = 0; col < m; col++) {
-            if ((row === 0 && col === 0) || dp[row][col] === -1) {
-                continue;
-            }
-
-            if (row === 0) {
-                if (dp[row][col - 1] === -1) {
-                    continue;
-                }
-    
-                dp[row][col] = dp[row][col - 1];
-                
-                continue;
-            }
+//         if (x !== m - 1 && currentVisit[x + 1][y] === 0) {
+//             const newVisit = currentVisit[x + 1][y] = 1;
             
-            if (col === 0) {
-                if (dp[row - 1][col] === -1) {
-                    continue;
-                }
-                
-                dp[row][col] = dp[row - 1][col];
-
-                continue;
-            }
+//             search.push([[x, y + 1], currentCount + 1, newVisit]);
+//         }
+        
+//         if (y !== n - 1 && currentVisit[x][y + 1] === 0) {
+//             const newVisit = currentVisit[x][y + 1] = 1;
             
-
-            if (dp[row - 1][col] === -1 || dp[row][col - 1] === -1) {
-                const maxNumber = Math.max(dp[row - 1][col], dp[row][col - 1]);
-                
-                dp[row][col] = maxNumber === -1 ? 0 : maxNumber % 1000000007;
-                
-                continue;
-            }
-            
-            dp[row][col] = (dp[row][col - 1] + dp[row - 1][col]) % 1000000007;
-
-            continue;
-        }
-    }
-
-    return dp[n - 1][m - 1];
+//             search.push([[x, y + 1], currentCount + 1, newVisit]);
+//         }
+        
+//         if (search.length === 0) {
+//             moveCount = Math.min(currentCount, moveCount);
+//         }
+//     }
+    
+    console.log("moveCount", moveCount);
 }
