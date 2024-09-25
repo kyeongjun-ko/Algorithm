c73ccabca3bb6d1dd7b9026a5a0314cd7a95691e
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-09-25 16:39:26 +0900

    [level 3] Title: 등굣길, Time: 3.75 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,30 +1,25 @@
 function solution(m, n, puddles) {
-    const visited = new Array(n).fill([...new Array(m)].fill(0));
-    console.log("visited", visited)
-    const search = [ [[0,0], 0, visited] ];
-    let moveCount = Infinity;
+    const dp = [...new Array(n)].map(e => new Array(m).fill(0));
 
-//     while (search.length !== 0) {
-//         const target = search.shift();
-//         console.log("target", target);
-//         const [[x, y], currentCount, currentVisit] = target;
+    for (let i = 0; i < puddles.length; i++) {
+        [dangerCol, dangerRow] = puddles[i];
         
-//         if (x !== m - 1 && currentVisit[x + 1][y] === 0) {
-//             const newVisit = currentVisit[x + 1][y] = 1;
-            
-//             search.push([[x, y + 1], currentCount + 1, newVisit]);
-//         }
-        
-//         if (y !== n - 1 && currentVisit[x][y + 1] === 0) {
-//             const newVisit = currentVisit[x][y + 1] = 1;
-            
-//             search.push([[x, y + 1], currentCount + 1, newVisit]);
-//         }
-        
-//         if (search.length === 0) {
-//             moveCount = Math.min(currentCount, moveCount);
-//         }
-//     }
+        dp[dangerRow - 1][dangerCol - 1] = -1;
+    };
     
-    console.log("moveCount", moveCount);
+    dp[0][0] = 1;
+    
+    for (let row = 0; row < n; row++) {
+        for (let col = 0; col < m; col++) {
+            let path = dp[row][col];
+            if (path === -1) continue;
+            
+            if (row - 1 >= 0 && dp[row - 1][col] !== -1) path += dp[row - 1][col];
+            if (col - 1 >= 0 && dp[row][col - 1] !== -1) path += dp[row][col - 1];
+            
+            dp[row][col] = path % 1000000007;
+        }
+    }
+
+    return dp[n-1][m-1];
 }
