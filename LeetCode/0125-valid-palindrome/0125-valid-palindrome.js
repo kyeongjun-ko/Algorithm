d0351f0fc86551c7f105e3f6887885c0fccbe224
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-09-28 23:57:02 +0900

    Time: 64 ms (52.82%), Space: 51.7 MB (75.39%) - LeetHub

---
+++ LeetCode/0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,11 @@
+var isPalindrome = function(s) {
+  s=  s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+  let rev="";
+  for(let i=0;i<s.length;i++){
+rev= s[i]+rev
+  }
+  if(rev==s)return true;
+  
+  return false;
+
+};
