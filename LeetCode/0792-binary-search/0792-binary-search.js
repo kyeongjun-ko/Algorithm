1b6951d78ee6a5dae50d6500a4477fd51c0a7500
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-10-02 17:26:37 +0900

    Time: 384 ms (5.08%), Space: 59.8 MB (5.79%) - LeetHub

--- LeetCode/0792-binary-search/0792-binary-search.js
+++ LeetCode/0792-binary-search/0792-binary-search.js
@@ -4,5 +4,36 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target)
+    // 초기 접근방식 - O(n)
+    // return nums.indexOf(target)
+
+    // 개선 접근방식 - 이진탐색    
+    let left = 0;
+    let right = nums.length;
+    let mid;
+
+    while (left !== right) {
+        mid = Math.floor((left + right) / 2);
+        console.log("mid", mid);
+
+        if (nums[mid] === target) return mid;
+
+        if (nums[mid] > target) {
+            right--;
+
+            continue;
+        }
+
+        if (nums[mid] < target) {
+            left++;
+
+            continue;
+        }
+
+        break;
+    }
+
+    return -1;
+
+    
 };
