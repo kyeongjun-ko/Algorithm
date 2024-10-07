14f53ec0b86d1668ca8d473d974f8913b00c37fa
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-10-07 10:51:43 +0900

    Time: 55 ms (92.2%), Space: 54.7 MB (26.3%) - LeetHub

---
+++ LeetCode/0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+
+/**
+ */
+
+
+const getDepth = (node) => {
+    if (node === null) {
+        return 0;                                                                              
+    }
+
+    const leftDepth = getDepth(node.left);
+    const rightDepth = getDepth(node.right);
+
+    if (leftDepth === -1 || rightDepth === -1 || Math.abs(leftDepth - rightDepth) > 1) {
+        return -1
+    }
+
+    return Math.max(rightDepth, leftDepth) + 1;
+}
+
+
+var isBalanced = function(root) {
+    return getDepth(root) !== -1;
+};
