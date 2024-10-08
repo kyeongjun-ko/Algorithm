aef84e65095ec0d9026524879cc42bd93473cf2f
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-10-08 11:47:38 +0900

    Time: 73 ms (56.26%), Space: 53 MB (40.88%) - LeetHub

--- LeetCode/0383-ransom-note/0383-ransom-note.js
+++ LeetCode/0383-ransom-note/0383-ransom-note.js
@@ -4,37 +4,32 @@
  * @return {boolean}
  */
 
-var canConstruct = function(ransomNote, magazine) {
-    const wordMap = {};
+var canConstruct = function (ransomNote, magazine) {
+	const wordMap = {};
 
-    for (let i = 0; i < magazine.length; i++) {
-        const target = magazine[i];
+	for (let i = 0; i < magazine.length; i++) {
+		const target = magazine[i];
 
-        if (wordMap[target]) { 
-            wordMap[target] += 1;
-        } else {
-            wordMap[target] = 1;
-        }
-    }
+		if (wordMap[target]) {
+			wordMap[target] += 1;
+		} else {
+			wordMap[target] = 1;
+		}
+	}
 
-    for (let i = 0; i < ransomNote.length; i++) {
-        const target = ransomNote[i];
+	for (let i = 0; i < ransomNote.length; i++) {
+		const target = ransomNote[i];
 
-        if (wordMap[target]) {
-            wordMap[target] -= 1;
+		if (wordMap[target]) {
+			wordMap[target] -= 1;
 
-            if (wordMap[target] === 0) delete wordMap[target];
-        } else {
-            return false;
-        }
-    }
+			if (wordMap[target] === 0) delete wordMap[target];
 
-    return true;
- };
+			continue;
+		}
 
-/**
-1. 일단 주어진 magazine를 순회하면서 Map에 저장한다.
-2. 그리고 ransomNote를 순회하면서 Map에 저장된걸 하나씩 뺀다.
-3. 맵의 사이즈가 1이상이면은 False를 반환한다.
+		return false;
+	}
 
- */
+	return true;
+};
