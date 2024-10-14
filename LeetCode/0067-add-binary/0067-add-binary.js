4942fa602f14efcd506d4e4a942cdd4257907d5b
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-10-14 11:21:01 +0900

    Time: 51 ms (82.35%), Space: 50.8 MB (57.08%) - LeetHub

--- LeetCode/0067-add-binary/0067-add-binary.js
+++ LeetCode/0067-add-binary/0067-add-binary.js
@@ -5,53 +5,17 @@
  */
 
 var addBinary = function(a, b) {
-    // const formatBianry = binary => {
-    //     const binaryList = [...binary].slice();
-    //     let digits = 0;
-    //     let decimal = 0;
-
-    //     while (binaryList.length !== 0) {
-    //         const target = binaryList.pop();
-
-    //         if (target !== "0") {
-    //             const number = 2 ** digits;
-    //             decimal += number;
-    //         }
-
-    //         digits += 1;
-    //     }
-
-    //     return decimal;
-    // };
-
     const formatBinary = binary => {
         return BigInt(`0b${binary}`);
     };
 
-    // const formatDecimal = decimal => {
-    //     let number = Number(decimal);
-    //     let binary = "";
-    
-    //     if (decimal === 0) {
-    //         return decimal.toString();
-    //     }
-
-    //     while (number > 0) {
-    //         const remainder = number % 2;
-
-    //         number = Math.floor(number / 2);
-    //         binary = remainder + binary;
-    //     }
-
-    //     return binary;
-    // }
-
     const formatDecimal = decimal => {
+        let binary = "";
+
         if (decimal === 0n) {
             return "0";
         }
 
-        let binary = "";
         while (decimal > 0n) {
             binary = (decimal % 2n).toString() + binary;
             decimal = decimal / 2n;
@@ -64,5 +28,4 @@ var addBinary = function(a, b) {
     const bigintB = formatBinary(b);
 
     return formatDecimal(bigintA + bigintB);
-   
 };
