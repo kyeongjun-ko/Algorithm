32b995cb3a311446ee4b8adc3e478b401e2831cd
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2025-01-30 13:44:09 +0900

    Time: 83 ms (41.33%), Space: 58.1 MB (40.59%) - LeetHub

--- LeetCode/0200-number-of-islands/0200-number-of-islands.js
+++ LeetCode/0200-number-of-islands/0200-number-of-islands.js
@@ -2,38 +2,38 @@
  * @param {character[][]} grid
  * @return {number}
  */
+
+// '1'(육지)과 '0'(물)의 지도를 나타내는 m x n 2D 이진 격자 격자가 주어질 때, 섬의 수를 반환합니다. 섬은 물로 둘러싸여 있으며 인접한 육지를 가로 또는 세로로 연결하여 형성됩니다. 그리드의 네 모서리가 모두 물로 둘러싸여 있다고 가정할 수 있습니다.
+
 var numIslands = function(grid) {
     const row = grid.length;
     const col = grid[0].length;
-
-    let numIsland = 0;
+    const range = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    let count = 0;
 
     const dfs = (x, y) => {
-        const search = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+        grid[x][y] = "0";
 
-        search.forEach(coord => {
-            const [dx, dy] = coord;
-            const newX = x + dx;
-            const newY = y + dy;
+        range.forEach(([dx, dy]) => {
+            const newX = dx + x;
+            const newY = dy + y;
 
-            if (0 <= newX && newX < row && 0 <= newY && newY < col) {
-                if (grid[newX][newY] === "1") {
-                    grid[newX][newY] = "0";
-                    dfs(newX, newY);
-                }
-            }
+            const isValid = newX >= 0 && newX < row && newY >= 0 && newY < col;
+
+            if (isValid && grid[newX][newY] === "1") dfs(newX, newY);
         });
-    }
+    };
 
-    
     for (let r = 0; r < row; r++) {
         for (let c = 0; c < col; c++) {
             if (grid[r][c] === "1") {
                 dfs(r, c);
-                numIsland++;
-            }
+                count++;
+            };        
         }
     }
 
-    return numIsland;
+    console.log("count", count);
+
+    return count;
 };
